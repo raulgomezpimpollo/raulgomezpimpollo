c265c72864486d339f43f48b64ceda07e6b1d2d0
Raúl Gómez <116291792+raulgomezpimpollo@users.noreply.github.com>
2023-11-14 11:59:18 +0100

    Create styles.cc

---
+++ styles.cc
@@ -0,0 +1,40 @@
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #f0f0f0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px 0;
+}
+
+nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav a {
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+}
+
+.container {
+    width: 50%;
+    margin: 50px auto;
+    text-align: center;
+}
+
+h1, h2 {
+    color: #333;
+}
+
